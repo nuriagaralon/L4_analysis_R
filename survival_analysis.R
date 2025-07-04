63a2a83bfd6808471710999cee46b3bd3f5a9b66
nuriagaralon <60143046+nuriagaralon@users.noreply.github.com>
2025-07-04 11:44:05 +0200

    Update survival to include replicate

--- survival_analysis.R
+++ survival_analysis.R
@@ -2,17 +2,33 @@ library(tidyverse)
 library(readxl)
 library(survival)
 library(survminer)
+library(plotly)
 
 # Get surv data file
 setwd("C:\\Users\\ngarriga\\Documents\\SydLab-One\\L4_analysis_R")
 
 path_surv <- list.files("data", pattern = "survival", full.names = TRUE)
 
-# Load old table, add headers
-# headers <- read.csv(path_surv, nrows = 2, header = FALSE)
-# headers <- sapply(headers, paste, collapse = "_")
-# table_surv <- read.csv(file = path_surv, skip = 2, header = FALSE)
-# names(table_surv) <- substr(headers, 11, 100)
+# Pivot function
+surv_pivot <- function(surv_data){
+  surv_table <- surv_data |>
+    pivot_longer(
+      cols = everything(),
+      names_to = c("condition", "type"),
+      names_pattern = "(.*)_(time|event)",
+      values_to = "value"
+    ) |>
+    pivot_wider(
+      names_from = type,
+      values_from = value,
+      values_fn = list
+    ) |>
+    unnest(c(time, event)) |>
+    drop_na()
+  surv_table$event <- as.numeric(surv_table$event)
+  surv_table$time <- as.numeric(surv_table$time)
+  surv_table
+}
 
 # Load new table, fix headers
 
@@ -29,28 +45,49 @@ all_tables <- map(path_surv, function(path){
   table_surv
 })
 
+# Check if the replicates (files in path_surv) are pool-able
+# by a survival log-rank test (if p > 0.05, they are similar enough)
+control <- c("Water", "N2")
+if(length(path_surv) > 1){
+  cont_tab <- map2(all_tables, seq_along(all_tables), function(table, num){
+    tab <- table |> select(contains(control[1])) |> select(contains(control[2]))
+    tab <- tab |> setNames(paste0(num, names(tab)))
+    tab
+  })
+
+  max_rows <- max(map_int(cont_tab, nrow))
+
+  cont_tab <- map(cont_tab, function(df){
+    if(nrow(df) < max_rows){
+      df[(nrow(df) + 1):max_rows, ] <- NA
+    }
+    df
+  })
+
+  cont_tab <- bind_cols(cont_tab)
+  data_con <- surv_pivot(cont_tab)
+  cont_lrt <- survdiff(Surv(time, event) ~ condition, data = data_con)
+
+  if(cont_lrt$pvalue < 0.05){
+    control_plot <- ggsurvplot(
+      survfit(Surv(time, event) ~ condition, data = data_con),
+      data = data_con,
+      conf.int = TRUE,
+      pval = TRUE,
+      xlab = "Time (hour)",
+      legend.title = ""
+    )
+    print(control_plot)
+    stop(paste("Replicates are too different, LRT pvalue =", cont_lrt$pvalue))
+  }
+}
+
+# bind rows
 table_surv <- bind_rows(all_tables)
 
-# Sort data to plot
-data_surv <- table_surv |>
-  pivot_longer(
-    cols = everything(),
-    names_to = c("condition", "type"),
-    names_pattern = "(.*)_(time|event)",
-    values_to = "value"
-  ) |>
-  pivot_wider(
-    names_from = type,
-    values_from = value,
-    values_fn = list
-  ) |>
-  unnest(c(time, event)) |>
-  drop_na()
-
-data_surv$event <- as.numeric(data_surv$event)
-data_surv$time <- as.numeric(data_surv$time)
-
 # Plot survival
+data_surv <- surv_pivot(table_surv)
+
 conds <- paste(c("Water", "S-medium"), collapse = "|")
 
 data_surv_set <- data_surv |> filter(str_detect(condition, conds))
@@ -59,23 +96,12 @@ km_fit <- survfit(Surv(time, event) ~ condition,
                   data = data_surv, conf.type = "log")
 
 km_fit_set <- survfit(Surv(time, event) ~ condition,
-                  data = data_surv_set, conf.type = "log")
+                      data = data_surv_set, conf.type = "log")
 
 # This calculates p-values and plots ONLY what is filtered through conds
 surv_plot_set <- ggsurvplot(
   km_fit_set,
   data = data_surv_set,
-  conf.int = TRUE,
-  pval = TRUE,
-  xlab = "Time (hour)",
-  legend.title = "",
-  legend.labs = levels(factor(data_surv_set$condition))
-)
-
-# This calculates p-values with everything, and plots conds
-surv_plot_all_filtered <- ggsurvplot(
-  km_fit_set,
-  data = data_surv,
   conf.int = TRUE,
   pval = TRUE,
   xlab = "Time (hour)",
@@ -92,8 +118,14 @@ surv_plot_all <- ggsurvplot(
   pval = TRUE,
   xlab = "Time (hour)",
   legend.title = "",
-  legend.labs = levels(factor(data_surv$condition))
+  legend.labs = levels(factor(data_surv$condition)),
+  palette = "viridis"
 )
+
+surv_plotly <- ggplotly(surv_plot_all[[1]])
+
+htmlwidgets::saveWidget(as_widget(surv_plotly), "results/survival.html")
+
 
 # Save plot
 ggsave_workaround <- function(g){
@@ -103,7 +135,7 @@ ggsave_workaround <- function(g){
                                 ncensor.plot.height = NULL)
 }
 
-g_to_save <- ggsave_workaround(surv_plot)
+g_to_save <- ggsave_workaround(surv_plot_set)
 
-ggsave(filename = "results/survival_all.png", plot = g_to_save,
+ggsave(filename = "results/survival_set.png", plot = g_to_save,
        width = 17, height = 15, dpi = 1000, units = "cm")
